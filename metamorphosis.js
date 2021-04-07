98dd4ef74c21f0e024f32c744ce84437a094c3fa
KriszTuri <turi.krisztina1999@gmail.com>
2021-04-07 21:46:51 +0200

    Drag cards done

--- metamorphosis.js
+++ metamorphosis.js
@@ -2,9 +2,29 @@ initDragAndDrop();
 
 function initDragAndDrop() {
     shuffleCards();
-
     // Initialize drag & drop elements here
 
+    //get all elements by tag
+    const draggables = document.querySelectorAll('.card');
+    const containers = document.querySelectorAll('.card-slot');
+
+    //apply event listener to everything in 'draggables' (looping draggables)
+    draggables.forEach(draggable => {
+      //dragstart: when we start dragging, neccessary
+      draggable.addEventListener('dragstart', () => {
+        //adding new class to differenciate the ones we're dragging from the rest
+        draggable.classList.add('dragging')
+      })
+      //removing class as soon as we stopped dragging (so it won't stay half opaque)
+      draggable.addEventListener('dragend', () =>{
+        draggable.classList.remove('dragging')
+      })
+    });
+    containers.forEach(container => {
+      container.addEventListener('dragover', () => {
+        console.log('drag over')
+      })
+    })
 }
 
 function shuffleCards() {
